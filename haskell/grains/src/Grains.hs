6b72f69657d1baff955392b6fa26ad4d654bd894
Brian Carroll <brian.carroll.ireland@gmail.com>
2016-12-13 12:17:59 +0800

    Grains speed : recursion with TCO

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -9,6 +9,9 @@ Not actually sure how to benchmark this in Haskell! (...and no Wi-Fi right now t
 Notes:
  - Refactor the validity checks out of the 'total' calculation
  - Assume recursive summation is faster than calculating a power
+     Might not be a great assumption! Depends on CPU and compiled implementation of **
+ - Assume tail-call optimisation speeds up the compiled implementation
+     No need to spend time creating stack frames & popping back up the call stack
 
 -}
 
@@ -18,17 +21,22 @@ square input =
     if input <= 0 || input > 64 then
         Nothing
     else
-        Just $ quot (1 + accumulateSquares input) 2
+        Just $ quot (1 + accumulateSquares 0 1 input) 2
 
 
-accumulateSquares :: Integer -> Integer
-accumulateSquares n =
-    case n of
-        1 ->
-            1
-        _ ->
-            1 + 2*(accumulateSquares (n-1))
+-- recursive sum with tail call
+accumulateSquares :: Integer -> Integer -> Integer -> Integer
+accumulateSquares acc current finish =
+    let
+        newAcc = (2*acc)+1
+    in
+        case current == finish of
+            True ->
+                newAcc
+            False ->
+                accumulateSquares newAcc (current+1) finish
+
 
 total :: Integer
 total =
-    accumulateSquares 64
+    accumulateSquares 0 1 64
