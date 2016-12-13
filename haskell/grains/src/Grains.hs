6272e8056163f8b5ceca52a3235e77aad47737ed
Brian Carroll <brian.carroll.ireland@gmail.com>
2016-12-13 11:35:43 +0800

    Optimise Haskell Grains for speed

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -2,6 +2,27 @@ module Grains (square, total) where
 
 import Data.Maybe
 
+{-
+Speed-optimised version
+Not actually sure how to benchmark this in Haskell! (...and no Wi-Fi right now to find out)
+
+Notes:
+ - Refactor the validity checks out of the 'total' calculation
+ - Assume built-in ** is fastest way to do powers
+        (**) expects Floating typeclass
+ - Avoid type conversions where possible (generate list of square indices as float)
+ - Neither Float nor Double have enough accuracy to get the sum correct!
+        Makes sense, they only give us 22 or 44 "binary places" and we need 63
+        Only Integer can give this precision
+        Need to convert to Integer for every square, then sum the integers
+
+-}
+
+sq :: Float -> Integer
+sq n =
+    round $ 2 ** (n - 1.0)
+
+
 square :: Integer -> Maybe Integer
 square input =
     if input <= 0 then
@@ -9,14 +30,9 @@ square input =
     else if input > 64 then
         Nothing
     else
-        let
-            n :: Int
-            n =
-                fromInteger input
-        in
-            Just (product $ take (n-1) $ repeat 2)
+        Just (sq $ fromInteger input)
 
 
 total :: Integer
 total =
-    sum $ catMaybes $ map square [1..64] 
+    sum $ map sq [1.0..64.0]
