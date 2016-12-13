66f86377f3e91ab9553b79b5a8c06ff9d9a465da
Brian Carroll <brian.carroll.ireland@gmail.com>
2016-12-13 10:59:26 +0800

    Haskell Beer Song with minimum repetition

--- haskell/beer-song/src/Beer.hs
+++ haskell/beer-song/src/Beer.hs
@@ -1,5 +1,8 @@
 module Beer (song) where
 
+import Data.Char (toUpper)
+import Data.List (intercalate)
+
 -- song :: String
 -- song = "
 --        \2 bottles of beer on the wall, 2 bottles of beer.\n\
@@ -14,30 +17,39 @@ module Beer (song) where
 nBottles :: Int -> String
 nBottles n =
     case n of
+        -1 -> "99 bottles"
         0 -> "no more bottles"
         1 -> "1 bottle"
         _ -> (show n) ++ " bottles"
 
-generateLine :: String -> Int -> String
-generateLine lyrics n =
-    lyrics ++
-        (nBottles n) ++
-        " of beer on the wall, " ++
-        (nBottles n) ++
-        " of beer.\n" ++
-        "Take one down and pass it around, " ++
-        (nBottles (n-1)) ++
-        " of beer on the wall.\n\n"
+one n =
+    if n==1 then
+        "it"
+    else
+        "one"
 
-mostOfTheLines :: String
-mostOfTheLines =
-    foldl generateLine "" [99,98..2]
+
+thirdBitOfVerse n =
+    if n==0 then
+        "Go to the store and buy some more, "
+    else
+        "Take " ++ (one n) ++ " down and pass it around, "
+
+
+capitalize :: String -> String
+capitalize s =
+    (toUpper $ head s) : (tail s)
+
+
+generateLine :: Int -> String
+generateLine n =
+    (capitalize (nBottles n)) ++ " of beer on the wall, " ++
+    (nBottles n) ++ " of beer.\n" ++
+    (thirdBitOfVerse n) ++
+    (nBottles (n-1)) ++ " of beer on the wall.\n"
 
 
 song :: String
 song =
-    mostOfTheLines ++
-        "1 bottle of beer on the wall, 1 bottle of beer.\n" ++
-        "Take " ++ "it" ++ " down and pass it around, no more bottles of beer on the wall.\n\n" ++
-        "No more bottles of beer on the wall, no more bottles of beer.\n" ++
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    intercalate "\n" $
+        map generateLine [99,98..0]
