b417883a8a3edaa8f48eb508b1ab7602c5146fc2
Brian Carroll <brian.carroll.ireland@gmail.com>
2016-12-13 12:05:57 +0800

    Grains speed idea - recursive addition

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -8,19 +8,9 @@ Not actually sure how to benchmark this in Haskell! (...and no Wi-Fi right now t
 
 Notes:
  - Refactor the validity checks out of the 'total' calculation
- - Assume built-in ** is fastest way to do powers
-        (**) expects Floating typeclass
- - Avoid type conversions where possible (generate list of square indices as float)
- - Neither Float nor Double have enough accuracy to get the sum correct!
-        Makes sense, they only give us 22 or 44 "binary places" and we need 63
-        Only Integer can give this precision
-        Need to convert to Integer for every square, then sum the integers
+ - Assume recursive summation is faster than calculating a power
 
 -}
-
-sq :: Float -> Integer
-sq n =
-    round $ 2 ** (n - 1.0)
 
 
 square :: Integer -> Maybe Integer
@@ -28,9 +18,17 @@ square input =
     if input <= 0 || input > 64 then
         Nothing
     else
-        Just (sq $ fromInteger input)
+        Just $ quot (1 + accumulateSquares input) 2
 
+
+accumulateSquares :: Integer -> Integer
+accumulateSquares n =
+    case n of
+        1 ->
+            1
+        _ ->
+            1 + 2*(accumulateSquares (n-1))
 
 total :: Integer
 total =
-    sum $ map sq [1.0..64.0]
+    accumulateSquares 64
