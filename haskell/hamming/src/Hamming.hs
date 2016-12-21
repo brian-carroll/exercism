542ad55d17c71ef020a61b26253af5106dac4b1e
Brian Carroll <brian.carroll.ireland@gmail.com>
2016-12-21 20:36:48 +0800

    Refactor Hamming

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,27 +1,28 @@
 module Hamming (distance) where
 
+helper :: String -> String -> Int -> Int
+helper a b dist =
+    case (a, b) of
+        ([], _) ->
+            dist
+
+        (_, []) ->
+            dist
+
+        (ha:ta, hb:tb) ->
+            let
+                mismatch =
+                    if ha == hb then
+                        0
+                    else
+                        1
+            in
+                helper ta tb (dist+mismatch)
+
+
 distance :: String -> String -> Maybe Int
 distance a b =
-    let
-        helper a b dist =
-            case (a, b) of
-                ([], _) ->
-                    dist
-
-                (_, []) ->
-                    dist
-
-                (ha:ta, hb:tb) ->
-                    let
-                        mismatch =
-                            if ha == hb then
-                                0
-                            else
-                                1
-                    in
-                        helper ta tb (dist+mismatch)
-    in
-        if length a /= length b then
-            Nothing
-        else
-            Just (helper a b 0)
+    if length a /= length b then
+        Nothing
+    else
+        Just (helper a b 0)
