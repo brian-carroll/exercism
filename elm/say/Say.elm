0fe422739a5d48134e73e263577bd6fcc5aef5f5
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-01-24 16:13:15 +0000

    Upgrade to 0.18. Run elm-format. Make graph of length of English words for numbers

--- elm/say/Say.elm
+++ elm/say/Say.elm
@@ -11,32 +11,72 @@ type SayError
 under10 : String -> String
 under10 digit =
     case digit of
-        "1" -> "one"
-        "2" -> "two"
-        "3" -> "three"
-        "4" -> "four"
-        "5" -> "five"
-        "6" -> "six"
-        "7" -> "seven"
-        "8" -> "eight"
-        "9" -> "nine"
-        _ -> ""
+        "1" ->
+            "one"
+
+        "2" ->
+            "two"
+
+        "3" ->
+            "three"
+
+        "4" ->
+            "four"
+
+        "5" ->
+            "five"
+
+        "6" ->
+            "six"
+
+        "7" ->
+            "seven"
+
+        "8" ->
+            "eight"
+
+        "9" ->
+            "nine"
+
+        _ ->
+            ""
 
 
 teens : String -> String
 teens digits =
     case digits of
-        "10" -> "ten"
-        "11" -> "eleven"
-        "12" -> "twelve"
-        "13" -> "thirteen"
-        "14" -> "fourteen"
-        "15" -> "fifteen"
-        "16" -> "sixteen"
-        "17" -> "seventeen"
-        "18" -> "eighteen"
-        "19" -> "nineteen"
-        _ -> ""
+        "10" ->
+            "ten"
+
+        "11" ->
+            "eleven"
+
+        "12" ->
+            "twelve"
+
+        "13" ->
+            "thirteen"
+
+        "14" ->
+            "fourteen"
+
+        "15" ->
+            "fifteen"
+
+        "16" ->
+            "sixteen"
+
+        "17" ->
+            "seventeen"
+
+        "18" ->
+            "eighteen"
+
+        "19" ->
+            "nineteen"
+
+        _ ->
+            ""
 
 
 under100 : String -> String
@@ -52,17 +92,38 @@ under100 digits =
                 "-" ++ under10 d
     in
         case (String.left 1 digits) of
-            "0" -> under10 lastDigit
-            "1" -> teens digits
-            "2" -> "twenty" ++ (lastWord lastDigit)
-            "3" -> "thirty" ++ (lastWord lastDigit)
-            "4" -> "forty" ++ (lastWord lastDigit)
-            "5" -> "fifty" ++ (lastWord lastDigit)
-            "6" -> "sixty" ++ (lastWord lastDigit)
-            "7" -> "seventy" ++ (lastWord lastDigit)
-            "8" -> "eighty" ++ (lastWord lastDigit)
-            "9" -> "ninety" ++ (lastWord lastDigit)
-            _ -> ""
+            "0" ->
+                under10 lastDigit
+
+            "1" ->
+                teens digits
+
+            "2" ->
+                "twenty" ++ (lastWord lastDigit)
+
+            "3" ->
+                "thirty" ++ (lastWord lastDigit)
+
+            "4" ->
+                "forty" ++ (lastWord lastDigit)
+
+            "5" ->
+                "fifty" ++ (lastWord lastDigit)
+
+            "6" ->
+                "sixty" ++ (lastWord lastDigit)
+
+            "7" ->
+                "seventy" ++ (lastWord lastDigit)
+
+            "8" ->
+                "eighty" ++ (lastWord lastDigit)
+
+            "9" ->
+                "ninety" ++ (lastWord lastDigit)
+
+            _ ->
+                ""
 
 
 under1000 : String -> String
@@ -74,27 +135,33 @@ under1000 digits =
         lastTwoDigits =
             under100 (String.right 2 digits)
     in
-        case (hundreds, lastTwoDigits) of
-            ("", "") ->
+        case ( hundreds, lastTwoDigits ) of
+            ( "", "" ) ->
                 ""
-            ("", _) ->
+
+            ( "", _ ) ->
                 lastTwoDigits
-            (_, "") ->
+
+            ( _, "" ) ->
                 hundreds ++ " hundred"
-            (_, _) ->
-                hundreds ++ " hundred and " ++ lastTwoDigits 
+
+            ( _, _ ) ->
+                hundreds ++ " hundred and " ++ lastTwoDigits
 
 
 combineScales : String -> String -> String -> String
 combineScales factor big small =
-    case (big, small) of
-        ("", "") ->
+    case ( big, small ) of
+        ( "", "" ) ->
             ""
-        ("", _) ->
+
+        ( "", _ ) ->
             small
-        (_, "") ->
+
+        ( _, "" ) ->
             big ++ " " ++ factor
-        (_, _) ->
+
+        ( _, _ ) ->
             big ++ " " ++ factor ++ " " ++ small
 
 
@@ -112,19 +179,19 @@ say x =
                 String.padLeft 12 '0' (toString x)
 
             last3 =
-                x `rem` 1000
+                rem x 1000
 
             -- ugly exceptional case!!
             extraAnd =
-                if (last3 < 100) && (last3 > 0) && (x>999) then
+                if (last3 < 100) && (last3 > 0) && (x > 999) then
                     "and "
                 else
-                    "" 
+                    ""
 
             words =
                 (extraAnd ++ under1000 (String.slice 9 12 digits))
                     |> combineScales "thousand" (under1000 (String.slice 6 9 digits))
-                    |> combineScales "million"  (under1000 (String.slice 3 6 digits))
-                    |> combineScales "billion"  (under1000 (String.slice 0 3 digits))
+                    |> combineScales "million" (under1000 (String.slice 3 6 digits))
+                    |> combineScales "billion" (under1000 (String.slice 0 3 digits))
         in
             Ok words
